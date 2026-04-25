eb4076002267ca051e5403d8e7e9ee8aa26bd856
mohammed <m.alali14@icloud.com>
2026-04-25 12:08:07 +0200

    Add project files

---
+++ src/test/java/come/codewithus/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package come.codewithus.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
